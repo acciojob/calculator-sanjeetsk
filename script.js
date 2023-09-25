95def25e68e6a5692470b21a9d2537047d77871b
sanjeetsk <ssanjeetkumar67@gmail.com>
2023-09-25 19:06:01 +0530

    added

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 //your code here
+let input = document.getElementById('input');
+
+let value = "";
+function buttonClick(e) {
+    const clickedButton = e.target.innerHTML;
+    value += clickedButton;
+    input.value = value;
+}
+
+function buttonClear() {
+    value = "";
+    input.value = ""
+}
+
+function buttonDel() {
+    if(value != ""){
+        value = value.toString();
+        const newString = value.slice(0, -1);
+        value=newString;
+        input.value = value;
+    }
+    else{
+        setTimeout(() => input.value="No input", 1500);
+    }
+}
+
+function buttonAns() {
+    try{
+        let ans = eval(value)
+        value = ans;
+        input.value = ans;
+    }
+    catch{
+        input.value = "Invalid Input"
+        setTimeout(() => value="", 1500);
+    }
+}
+
